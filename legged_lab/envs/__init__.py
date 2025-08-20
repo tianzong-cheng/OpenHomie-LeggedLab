4a4ccfeaf04cb48fc6977fe41e037bffd1fc8793
Tianzong Cheng <chengtianzong@sjtu.edu.cn>
2025-08-20 15:31:51 +0800

    feat: environment registration

--- legged_lab/envs/__init__.py
+++ legged_lab/envs/__init__.py
@@ -12,29 +12,7 @@
 
 from legged_lab.envs.base.base_env import BaseEnv
 from legged_lab.envs.base.base_env_config import BaseAgentCfg, BaseEnvCfg
-from legged_lab.envs.g1.g1_config import (
-    G1FlatAgentCfg,
-    G1FlatEnvCfg,
-    G1RoughAgentCfg,
-    G1RoughEnvCfg,
-)
-from legged_lab.envs.gr2.gr2_config import (
-    GR2FlatAgentCfg,
-    GR2FlatEnvCfg,
-    GR2RoughAgentCfg,
-    GR2RoughEnvCfg,
-)
-from legged_lab.envs.h1.h1_config import (
-    H1FlatAgentCfg,
-    H1FlatEnvCfg,
-    H1RoughAgentCfg,
-    H1RoughEnvCfg,
-)
+from legged_lab.envs.g1.g1_config import G1HomieAgentCfg, G1HomieEnvCfg
 from legged_lab.utils.task_registry import task_registry
 
-task_registry.register("h1_flat", BaseEnv, H1FlatEnvCfg(), H1FlatAgentCfg())
-task_registry.register("h1_rough", BaseEnv, H1RoughEnvCfg(), H1RoughAgentCfg())
-task_registry.register("g1_flat", BaseEnv, G1FlatEnvCfg(), G1FlatAgentCfg())
-task_registry.register("g1_rough", BaseEnv, G1RoughEnvCfg(), G1RoughAgentCfg())
-task_registry.register("gr2_flat", BaseEnv, GR2FlatEnvCfg(), GR2FlatAgentCfg())
-task_registry.register("gr2_rough", BaseEnv, GR2RoughEnvCfg(), GR2RoughAgentCfg())
+task_registry.register("g1_homie", BaseEnv, G1HomieEnvCfg(), G1HomieAgentCfg())
